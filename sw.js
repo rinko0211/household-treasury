ab0feef5570165290d442f74270471bc193529ee
rinko0211 <sunnyground0203@gmail.com>
2026-09-02 23:40:54 +0900

    Cache application logic for offline use

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE="household-treasury-v2";
-const CORE=["./","./index.html","./manifest.webmanifest","./sync.js","./icons/icon.svg"];
+const CORE=["./","./index.html","./manifest.webmanifest","./app.js","./sync.js","./icons/icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;const u=new URL(e.request.url);if(u.origin!==location.origin)return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const cp=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,cp));return resp}).catch(()=>caches.match("./index.html"))))});
